f158dc5bdb6ad3e55d0f18ea196bf3cc91eff9ca
Md Parvez <mohammedparvez1923@gmail.com>
2025-03-06 14:52:02 +0600

    -image upload lesson

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/users");
 const taskRouter = require("./routes/tasks");
-
+const multer=require("multer");
 
 
 
@@ -15,11 +15,39 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-
-
 //routers
 app.use("/users",userRouter)
 app.use("/tasks",taskRouter)
+
+
+//multer
+//multer
+const upload = multer({
+    dest: "./uploads/",
+    limits: {
+        fileSize: 1000*1000
+    },
+    fileFilter: (req, file, cb) => {
+        if(!file.originalname.match(/\.(doc|docx)$/)){
+            return cb(new Error("Only pdf files allowed"));
+        }
+        cb(undefined, true);
+    }
+})
+
+app.post('/upload', upload.single('upload'), (req, res)=>{
+    res.send()
+})
+
+
+
+
+
+
+
+
+
+
 
 // Sample API Route
 app.get("/", (req, res) => {
